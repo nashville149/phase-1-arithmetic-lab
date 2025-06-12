72ee2e1c5c7d9ef80b4d0e089af8e57abe062fed
nashville149 <milliamkarambu76@gmail.com>
2025-06-12 10:23:40 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+const num3 = 16;
+const num4 = 12;
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
+
+
+
+
 
 // Write your code here
